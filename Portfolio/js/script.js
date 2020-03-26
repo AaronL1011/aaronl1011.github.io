e4f4bbb34acb6eccbbcb73ea5b34865993381011
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-03-26 20:58:28 +1100

    added themes, fixed scrolling JS

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,15 +1,30 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.body;
+
+// Button Event Handlers
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+};
+
+lightButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+};
+
 $(".scrollTo").on('click', function(e) {
     e.preventDefault();
     var target = $(this).attr('href');
     $('html, body').animate({
       scrollTop: ($(target).offset().top)
-    }, 2000);
+    }, 1500);
  });
 
  $(window).scroll(function() { // when the page is scrolled run this
     if($(this).scrollTop() != 0) { // if you're NOT at the top
-        $('#returnImg').fadeIn("fast"); // fade in
+        $('#top').fadeIn("fast"); // fade in
     } else { // else
-        $('#returnImg').fadeOut("fast"); // fade out
+        $('#top').fadeOut("fast"); // fade out
     }
 });
+
